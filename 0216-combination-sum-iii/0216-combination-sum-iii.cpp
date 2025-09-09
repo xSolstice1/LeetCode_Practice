8057fc88a2d8fd46c4edd6cb0f8df520f502eb14
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-10 04:15:54 +0800

    Time: 0 ms (100%), Space: 8.7 MB (60.41%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void backtrack(vector<vector<int>>& res, vector<int>& curr, int start, int k, int n, vector<bool>& used) {
-        if (curr.size() == k && n == 0) {
+    void backtrack(vector<vector<int>>& res, vector<int>& curr, int k, int n, int start, int count, int sum) {
+        if (count==k && sum==n) {
             res.push_back(curr);
             return;
         }
+        if (count>k || sum>n) return;
 
         for (int i=start; i<=9; i++) {
-            if (used[i]) continue;
             curr.push_back(i);
-            used[i] = true;
-            backtrack(res,curr,i,k,n-i,used);
+            backtrack(res,curr,k,n,i+1,count+1,sum+i);
             curr.pop_back();
-            used[i] = false;
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> res;
         vector<int> curr;
-        vector<bool> used(9,false);
-
-        backtrack(res,curr,1,k,n,used);
-
+        backtrack(res,curr,k,n,1,0,0);
         return res;
     }
 };
