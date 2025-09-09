89496b3f8e37b0dd6172b7298ec00dccea913523
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-09 19:07:35 +0800

    Time: 0 ms (100%), Space: 8.6 MB (18.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    //fibonacci ways(n)=ways(n−1)+ways(n−2)
     int climbStairs(int n) {
-        int a=1,b=1;
-        
-        for (int i=0; i<n-1; i++) {
-            int c=a; //temp to store a
-            a+=b; //fib step (a+b)
-            b=c; //b = previous a
+        // int one = 1, two = 1;
+
+        // for (int i = 0; i < n - 1; ++i) {
+        //     int temp = one;
+        //     one = one + two;
+        //     two = temp;
+        // }
+
+        // return one;
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+
+        for (int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return a; //a = ways(n)
+        return dp[n];
     }
 };
