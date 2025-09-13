b3b05ac6b47007e485d8ce371ff28542f2df2533
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-14 01:15:41 +0800

    Time: 288 ms (83.42%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,8 +1,8 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 SELECT
     SCORE,
-    DENSE_RANK() OVER (ORDER BY SCORE DESC) AS RANK
+    DENSE_RANK() OVER (ORDER BY SCORE DESC) AS "RANK"
 FROM
     SCORES
 ORDER BY
-    RANK ASC;
+    "RANK" ASC;
