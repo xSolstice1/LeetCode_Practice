558ea4f6240360914e504b04829f25742a015e36
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-13 18:07:26 +0800

    Time: 79 ms (17.61%), Space: 71.6 MB (71.84%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        // bruteforce, TLE
+        // long long res = 0;
+
+        // for (int i=0; i<nums.size(); i++) {
+        //     for (int j=i+1; j<nums.size(); j++) {
+        //         if (lower <= nums[i] + nums[j] && nums[i] + nums[j] <= upper) {
+        //             res++;
+        //         }
+        //     }
+        // }
+        
+        // return res;
+
+        //binary search?
+        sort(nums.begin(),nums.end());
         long long res = 0;
 
         for (int i=0; i<nums.size(); i++) {
-            for (int j=i+1; j<nums.size(); j++) {
-                if (lower <= nums[i] + nums[j] && nums[i] + nums[j] <= upper) {
-                    res++;
-                }
-            }
+            auto lowerBound = lower_bound(nums.begin()+i+1, nums.end(), lower-nums[i]);
+            auto upperBound = upper_bound(nums.begin()+i+1, nums.end(), upper-nums[i]);
+
+            res += upperBound - lowerBound; //number of valid pairs
         }
-        
+
         return res;
     }
 };
