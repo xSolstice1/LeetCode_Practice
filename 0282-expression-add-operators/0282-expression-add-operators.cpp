7d8f1ed0a6108ccd610e2b149ec5b22ebb921fc4
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-13 16:51:52 +0800

    Time: 444 ms (22.05%), Space: 93.3 MB (43.63%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    void backtrack(string num, int target, vector<string>& res, string curr,
-    int index, long eval, long prev) {
-        if (index == num.size()) {
-            if (eval == target) {
-                res.push_back(curr);
+    void backtrack(string num, int target, string path, vector<string>& res,
+    int index, long curr, long prev) {
+        if (index==num.size()) {
+            if (curr==target) {
+                res.push_back(path);
             }
             return;
         }
 
         for (int i=index; i<num.size(); i++) {
-            if (i != index && num[index] == '0') break; //leading zero
+            if (num[index] == '0' && i != index) break; //leading 0
 
             string curSub = num.substr(index,i-index+1);
             long curEval = stol(curSub);
 
-            if (index == 0) {
-                backtrack(num,target,res,curSub,i+1,curEval,curEval);
+            if (index == 0) { //start
+                backtrack(num,target,path+curSub,res,i+1,curEval,curEval);
             } else {
-                // add
-                backtrack(num,target,res,curr+"+"+curSub,i+1,eval+curEval,curEval);
-                // minus
-                backtrack(num,target,res,curr+"-"+curSub,i+1,eval-curEval,-curEval);
-                // multiply
-                backtrack(num,target,res,curr+"*"+curSub,i+1,(eval-prev)+(prev*curEval),prev*curEval);
+                //backtrack 3 diff scenarios, +, -, *
+                backtrack(num,target,path+'+'+curSub,res,i+1,curr+curEval,curEval);
+                backtrack(num,target,path+'-'+curSub,res,i+1,curr-curEval,-curEval);
+                backtrack(num,target,path+'*'+curSub,res,i+1,(curr-prev)+(prev*curEval),curEval*prev);
             }
         }
     }
     vector<string> addOperators(string num, int target) {
         if (num.empty()) return {};
-
         vector<string> res;
-        backtrack(num,target,res,"",0,0,0);
+        backtrack(num,target,"",res,0,0,0);
+
         return res;
     }
 };
