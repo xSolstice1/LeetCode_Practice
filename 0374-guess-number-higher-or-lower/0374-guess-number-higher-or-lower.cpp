89a4fee01e35d2722ac0d767a031e0b5628f4756
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-16 00:17:21 +0800

    Time: 2 ms (54.48%), Space: 8 MB (4.41%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -12,7 +12,7 @@ public:
     int guessNumber(int& n) {
         int left = 1, right = n;
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right-left) / 2;
 
             if (guess(mid) == 0) {
@@ -24,6 +24,6 @@ public:
             }
         }
 
-        return left;
+        return -1;
     }
 };
