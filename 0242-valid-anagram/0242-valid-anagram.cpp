89157c89834361d2f4695df35f65cf152ed4734e
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-09-23 22:18:12 +0800

    Time: 0 ms (100%), Space: 9.7 MB (48.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> mp;
+        // can we use fixed size array instead? try approach 2
+        // unordered_map<char,int> mp;
 
+        // if (s.size() != t.size()) return false;
+
+        // for (int i=0; i<s.size(); i++) {
+        //     mp[s[i]]++;
+        //     mp[t[i]]--;
+        // }
+
+        // for (auto& itr:mp) {
+        //     if (itr.second != 0) return false;
+        // }
+
+        // return true;
+
+        // fixed array
         if (s.size() != t.size()) return false;
 
+        int count[26] = {0}; //26 alphabets a-z
+
         for (int i=0; i<s.size(); i++) {
-            mp[s[i]]++;
-            mp[t[i]]--;
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
         }
 
-        for (auto& itr:mp) {
-            if (itr.second != 0) return false;
+        for (int i=0; i<26; i++) {
+            if (count[i]!=0) return false;
         }
 
         return true;
