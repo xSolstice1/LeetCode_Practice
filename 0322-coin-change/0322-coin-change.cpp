5396112d1e702b98866117b798f46ab1fa7e0ff6
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-10-10 21:15:36 +0800

    Time: 17 ms (86.24%), Space: 17.9 MB (83.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,11 +4,9 @@ public:
         vector<int> dp(amount+1,amount+1);
         dp[0] = 0;
 
-        for (int i=1; i<amount+1; i++) {
+        for (int i=1; i<=amount; i++) {
             for (auto c:coins) {
-                if (i-c >= 0) {
-                    dp[i] = min(dp[i],dp[i-c]+1);
-                }
+                if (i-c>=0) dp[i] = min(dp[i],1+dp[i-c]);
             }
         }
 
