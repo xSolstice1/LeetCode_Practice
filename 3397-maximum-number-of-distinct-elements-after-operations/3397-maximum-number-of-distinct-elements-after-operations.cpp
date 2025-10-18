9371531a8513a160252af0fcb831f7a85b0ea6c3
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-10-19 03:57:52 +0800

    Time: 107 ms (94.29%), Space: 97.6 MB (68.89%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        unordered_set<int> used;
+        // unordered_set<int> used;
+        int res = 0;
         int curr = INT_MIN;
 
         for (int x:nums) {
             int candidate = max(x-k,curr+1);
             if (candidate <= x+k) {
-                used.insert(candidate);
+                res++;
                 curr = candidate;
             }
         }
 
-        return used.size();
+        return res;
     }
 };
