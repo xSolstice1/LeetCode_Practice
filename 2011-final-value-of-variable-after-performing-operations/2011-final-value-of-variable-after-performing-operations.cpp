b370dc0709e6b60255e77e65aad6a1a3db3cf294
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2025-10-20 16:46:12 +0800

    Time: 0 ms (100%), Space: 17.6 MB (42.92%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
+        // int res=0;
+
+        // for (string s:operations) {
+        //     if (s.find('-') != string::npos) {
+        //         res--;
+        //     } else {
+        //         res++;
+        //     }
+        // }
+
+        // return res;
+
         int res=0;
 
-        for (string s:operations) {
-            if (s.find('-') != string::npos) {
-                res--;
-            } else {
-                res++;
-            }
+        for (string& s:operations) {
+            res += s[1] == '-' ? -1:1;
         }
 
         return res;
