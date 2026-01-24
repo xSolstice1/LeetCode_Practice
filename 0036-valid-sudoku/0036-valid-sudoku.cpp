cc6381c918bd7505b0c7845abff398bcaf1959eb
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2026-01-24 21:45:25 +0800

    Time: 0 ms (100%), Space: 21.9 MB (68.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool row[9][9], col[9][9], box[9][9]; //define arrays for row/col/box
+        bool row[9][9], col[9][9], box[9][9];
 
         for (int i=0; i<board.size(); i++) {
             for (int j=0; j<board[0].size(); j++) {
-                if (board[i][j] == '.') continue; //skip iteration if cell is empty
+                if (board[i][j] == '.') continue;
+                char c = board[i][j] - '1';
+                int boxInd = (i/3) * 3 + (j/3);
 
-                int c = board[i][j] - '1';
-                int boxInd = (i/3) * 3 + (j/3); //get box index
+                if (row[i][c] || col[j][c] || box[boxInd][c]) return false;
 
-                if (row[i][c]||col[j][c]||box[boxInd][c]) { //if char is invalid, return false
-                    return false;
-                }
-
-                //set current char to be "found" in current row/col/box
                 row[i][c] = true;
                 col[j][c] = true;
                 box[boxInd][c] = true;
