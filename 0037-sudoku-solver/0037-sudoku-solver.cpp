27b220026f19cf5d081ae19b5b1cee821ffabda4
Ang Jin Wei <117270763+xSolstice1@users.noreply.github.com>
2026-01-25 20:47:23 +0800

    Time: 50 ms (94.04%), Space: 8.8 MB (31.06%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,128 +1,44 @@
 class Solution {
 private:
-    bool row[9][9] = {false};
-    bool col[9][9] = {false};
-    bool box[9][9] = {false};
+    bool row[9][9] = {false}, col[9][9] = {false}, box[9][9] = {false};
     vector<pair<int,int>> emptyCells;
-public:
-    // method 1: a little slow
-    // bool isValid(vector<vector<char>>& board, char ch, int r, int c) {
-    //     for (int i=0; i<board.size(); i++) {
-    //         int boxR = (r/3) * 3 + (i/3);
-    //         int boxC = (c/3) * 3 + (i%3);
 
-    //         if (board[r][i]==ch||board[i][c]==ch||board[boxR][boxC]==ch) return false;
-    //     }
+    bool backtrack(vector<vector<char>>& board, int step) {
+        if (step == emptyCells.size()) return true;
 
-    //     return true;
-    // }
-    // bool backtrack(vector<vector<char>>& board) {
-    //     for (int i=0; i<board.size(); i++) {
-    //         for (int j=0; j<board[0].size(); j++) {
-    //             if (board[i][j]=='.') {
-    //                 for (int ch='1';ch<='9';ch++) {
-    //                     if (isValid(board,ch,i,j)) {
-    //                         board[i][j]=ch;
-    //                         if (backtrack(board)) return true;
-    //                         board[i][j]='.';
-    //                     }
-    //                 }
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    
-    // method 2, even slower, optimize?
-    // bool backtrack(vector<vector<char>>& board, 
-    // vector<unordered_set<char>>& r, 
-    // vector<unordered_set<char>>& c, 
-    // vector<unordered_set<char>>& b,
-    // vector<pair<int,int>>& empty,
-    // int idx) {
-    //     if (idx==empty.size()) return true;
-
-    //     int i = empty[idx].first;
-    //     int j = empty[idx].second;
-    //     int k = (i/3)*3+(j/3);
-
-    //     for (char ch='1'; ch<='9'; ch++) {
-    //         if (r[i].count(ch)==0 && c[j].count(ch)==0 && b[k].count(ch)==0) {
-    //             board[i][j]=ch;
-    //             r[i].insert(ch);
-    //             c[j].insert(ch);
-    //             b[k].insert(ch);
-
-    //             if (backtrack(board,r,c,b,empty,idx+1)) return true;
-
-    //             board[i][j]='.';
-    //             r[i].erase(ch);
-    //             c[j].erase(ch);
-    //             b[k].erase(ch);
-    //         }
-    //     }
-
-    //     return false;
-    // }
-
-    // method 3
-    bool backtrack(vector<vector<char>>& board, int idx) {
-        if (idx==emptyCells.size()) return true;
-
-        auto [r,c] = emptyCells[idx];
-        int b = (r/3)*3+(c/3);
+        auto [r,c] = emptyCells[step];
+        int k = (r/3)*3+(c/3);
 
         for (int i=0; i<9; i++) {
-            if (!row[r][i]&&!col[c][i]&&!box[b][i]) {
-                board[r][c]='1'+i;
-                row[r][i]=col[c][i]=box[b][i]=true;
+            if (!row[r][i]&&!col[c][i]&&!box[k][i]) {
+                board[r][c] = i+'1';
+                row[r][i]=col[c][i]=box[k][i]=true;
 
-                if (backtrack(board,idx+1)) return true;
+                if (backtrack(board,step+1)) return true;
 
-                board[r][c]='.';
-                row[r][i]=col[c][i]=box[b][i]=false;
+                board[r][c] = '.';
+                row[r][i]=col[c][i]=box[k][i]=false;
             }
         }
 
         return false;
     }
 
+public:
     void solveSudoku(vector<vector<char>>& board) {
-        // method 1
-        // backtrack(board);
-
-        // method 2
-        // vector<unordered_set<char>> r(9),c(9),b(9);
-        // vector<pair<int,int>> empty;
-
-        // for (int i=0; i<9; i++) {
-        //     for (int j=0; j<9; j++) {
-        //         if (board[i][j]!='.') {
-        //             char ch=board[i][j];
-        //             r[i].insert(ch);
-        //             c[j].insert(ch);
-        //             b[(i/3)*3+(j/3)].insert(ch);
-        //         } else {
-        //             empty.push_back({i,j});
-        //         }
-        //     }
-        // }
-
-        // backtrack(board,r,c,b,empty,0);
-
-        // method 3
-        //initialize
         for (int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
                 if (board[i][j] == '.') {
                     emptyCells.push_back({i,j});
                 } else {
-                    int d = board[i][j] - '1';
-                    row[i][d] = col[j][d] = box[(i/3)*3 + j/3][d] = true;
+                    int ch = board[i][j] - '1';
+                    int k = (i/3)*3+(j/3);
+
+                    row[i][ch]=col[j][ch]=box[k][ch]=true;
                 }
             }
         }
-        backtrack(board, 0);
+
+        backtrack(board,0);
     }
 };
